79baf26bc74909f9efd005c5c32c75216215470e
Linn Thit Htoo <lth1212001@gmail.com>
2024-06-21 17:49:59 +0630

    -

---
+++ DotNet8WebApi.HexagonalWithResultPattern.Mapper/GlobalUsings.cs
@@ -0,0 +1,4 @@
+// Global using directives
+
+global using DotNet8WebApi.HexagonalWithResultPattern.DbService.AppDbContexts;
+global using DotNet8WebApi.HexagonalWithResultPattern.Models.Features.Blog;
