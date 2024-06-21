899a76691e8161f8a2ffeace13691e91a15b1b92
Linn Thit Htoo <lth1212001@gmail.com>
2024-06-21 17:52:36 +0630

    -

--- DotNet8WebApi.HexagonalWithResultPattern.Models/Features/Result.cs
+++ DotNet8WebApi.HexagonalWithResultPattern.Models/Features/Result.cs
@@ -1,34 +1,33 @@
-﻿namespace DotNet8WebApi.HexagonalWithResultPattern.Models.Features
+﻿namespace DotNet8WebApi.HexagonalWithResultPattern.Models.Features;
+
+public class Result<T>
 {
-    public class Result<T>
+    public bool Success { get; set; }
+    public string Message { get; set; }
+    public T Data { get; set; }
+
+    public static Result<T> SuccessResult(T data, string message = "Successful.")
     {
-        public bool Success { get; set; }
-        public string Message { get; set; }
-        public T Data { get; set; }
+        return new Result<T> { Data = data, Message = message, Success = true };
+    }
 
-        public static Result<T> SuccessResult(T data, string message = "Successful.")
-        {
-            return new Result<T> { Data = data, Message = message, Success = true };
-        }
+    public static Result<T> SuccessResult(string message = "Successful.")
+    {
+        return new Result<T> { Message = message, Success = true };
+    }
 
-        public static Result<T> SuccessResult(string message = "Successful.")
-        {
-            return new Result<T> { Message = message, Success = true };
-        }
+    public static Result<T> FailureResult(string message = "Fail.")
+    {
+        return new Result<T> { Message = message, Success = false };
+    }
 
-        public static Result<T> FailureResult(string message = "Fail.")
-        {
-            return new Result<T> { Message = message, Success = false };
-        }
+    public static Result<T> FailureResult(Exception ex)
+    {
+        return new Result<T> { Message = ex.ToString(), Success = false };
+    }
 
-        public static Result<T> FailureResult(Exception ex)
-        {
-            return new Result<T> { Message = ex.ToString(), Success = false };
-        }
-
-        public static Result<T> ExecuteResult(int result)
-        {
-            return result > 0 ? Result<T>.SuccessResult() : Result<T>.FailureResult();
-        }
+    public static Result<T> ExecuteResult(int result)
+    {
+        return result > 0 ? Result<T>.SuccessResult() : Result<T>.FailureResult();
     }
 }
