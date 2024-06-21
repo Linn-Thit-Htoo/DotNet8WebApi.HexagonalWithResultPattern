14bd8244f232bd2b283e58cd9ff4aa15466bef28
Linn Thit Htoo <lth1212001@gmail.com>
2024-06-21 17:50:19 +0630

    -

--- DotNet8WebApi.HexagonalWithResultPattern.Mapper/ChangeModel.cs
+++ DotNet8WebApi.HexagonalWithResultPattern.Mapper/ChangeModel.cs
@@ -1,26 +1,25 @@
-﻿namespace DotNet8WebApi.HexagonalWithResultPattern.Mapper
-{
-    public static class ChangeModel
-    {
-        public static BlogModel Change(this TblBlog dataModel)
-        {
-            return new BlogModel
-            {
-                BlogId = dataModel.BlogId,
-                BlogTitle = dataModel.BlogTitle,
-                BlogAuthor = dataModel.BlogAuthor,
-                BlogContent = dataModel.BlogContent
-            };
-        }
+﻿namespace DotNet8WebApi.HexagonalWithResultPattern.Mapper;
 
-        public static TblBlog Change(this BlogRequestModel requestModel)
+public static class ChangeModel
+{
+    public static BlogModel Change(this TblBlog dataModel)
+    {
+        return new BlogModel
         {
-            return new TblBlog
-            {
-                BlogTitle = requestModel.BlogTitle,
-                BlogAuthor = requestModel.BlogAuthor,
-                BlogContent = requestModel.BlogContent
-            };
-        }
+            BlogId = dataModel.BlogId,
+            BlogTitle = dataModel.BlogTitle,
+            BlogAuthor = dataModel.BlogAuthor,
+            BlogContent = dataModel.BlogContent
+        };
+    }
+
+    public static TblBlog Change(this BlogRequestModel requestModel)
+    {
+        return new TblBlog
+        {
+            BlogTitle = requestModel.BlogTitle,
+            BlogAuthor = requestModel.BlogAuthor,
+            BlogContent = requestModel.BlogContent
+        };
     }
 }
