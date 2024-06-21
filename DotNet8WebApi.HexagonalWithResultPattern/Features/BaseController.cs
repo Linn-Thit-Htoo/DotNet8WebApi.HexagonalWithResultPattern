3f45c6bc92fe98523e368b5d274ac3949aaa15f1
Linn Thit Htoo <lth1212001@gmail.com>
2024-06-21 17:46:24 +0630

    -

--- DotNet8WebApi.HexagonalWithResultPattern/Features/BaseController.cs
+++ DotNet8WebApi.HexagonalWithResultPattern/Features/BaseController.cs
@@ -1,17 +1,16 @@
-﻿namespace DotNet8WebApi.HexagonalWithResultPattern.Features
-{
-    [Route("api/[controller]")]
-    [ApiController]
-    public class BaseController : ControllerBase
-    {
-        protected IActionResult Content(object? obj)
-        {
-            return Ok(JsonConvert.SerializeObject(obj));
-        }
+﻿namespace DotNet8WebApi.HexagonalWithResultPattern.Features;
 
-        protected IActionResult InternalServerError(Exception ex)
-        {
-            return StatusCode(500, ex.Message);
-        }
+[Route("api/[controller]")]
+[ApiController]
+public class BaseController : ControllerBase
+{
+    protected IActionResult Content(object? obj)
+    {
+        return Ok(JsonConvert.SerializeObject(obj));
+    }
+
+    protected IActionResult InternalServerError(Exception ex)
+    {
+        return StatusCode(500, ex.Message);
     }
 }
