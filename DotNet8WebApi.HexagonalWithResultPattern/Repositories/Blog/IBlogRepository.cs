c06d5108053ab4f738cc33b8f2ad8ef36274e7e6
Linn Thit Htoo <lth1212001@gmail.com>
2024-06-21 17:47:19 +0630

    -

--- DotNet8WebApi.HexagonalWithResultPattern/Repositories/Blog/IBlogRepository.cs
+++ DotNet8WebApi.HexagonalWithResultPattern/Repositories/Blog/IBlogRepository.cs
@@ -1,11 +1,10 @@
-﻿namespace DotNet8WebApi.HexagonalWithResultPattern.Repositories.Blog
+﻿namespace DotNet8WebApi.HexagonalWithResultPattern.Repositories.Blog;
+
+public interface IBlogRepository
 {
-    public interface IBlogRepository
-    {
-        Task<Result<BlogListResponseModel>> GetBlogs();
-        Task<Result<BlogModel>> GetBlog(int id);
-        Task<Result<BlogResponseModel>> CreateBlog(BlogRequestModel requestModel);
-        Task<Result<BlogResponseModel>> PatchBlog(BlogRequestModel requestModel, int id);
-        Task<Result<BlogResponseModel>> DeleteBlog(int id);
-    }
+    Task<Result<BlogListResponseModel>> GetBlogs();
+    Task<Result<BlogModel>> GetBlog(int id);
+    Task<Result<BlogResponseModel>> CreateBlog(BlogRequestModel requestModel);
+    Task<Result<BlogResponseModel>> PatchBlog(BlogRequestModel requestModel, int id);
+    Task<Result<BlogResponseModel>> DeleteBlog(int id);
 }
